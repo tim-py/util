955398db9c40146da948e5a66a2522b9e8946a8e
Tim Martin <nobody@nowhere.org>
2020-08-01 21:37:25 -0400

    Added capability to print binding signatures for books with many pages.

--- pdf_booklet.py
+++ pdf_booklet.py
@@ -17,10 +17,38 @@ and making everything 4 times larger will fill out the booklet pages.
 
 The "amount" parameters (--hoffset, --width, --height) are delta adjustments and must be made
 using a unit suffix of: in, cm, or mm.
+
+The --signature parameter specifies the maximum number of pages in each signature (or binding group)
+and must be a multiple of 4. If omitted, the entire PDF will be printed as a single signature. By printing
+the content into multiple signatures, this allows larger books to be bound that would normally not be
+able to be folded over with so many pages.
 """
 opt = None
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("pdf_booklet")
+
+
+class PdfSignatureReader(PyPDF2.PdfFileReader):
+    """
+    Adds signature properties and methods to the PDFFileReader to allow reading page ranges defined by
+    each "signature" - Book binding term referring to page groups that are bound together.
+    """
+
+    def __init__(self, start_page, end_page, *args, **kwargs):
+        self.signature_start = start_page
+        self.signature_end = end_page
+        super().__init__(*args, **kwargs)
+
+    @property
+    def signature_page_count(self):
+        return (self.signature_end - self.signature_start) + 1
+
+    def get_signature_page(self, pageNumber):
+        physical_page = pageNumber + self.signature_start
+        logger.debug(f"Signature page number called with {pageNumber} mapped to physical page {physical_page}")
+        if physical_page < self.signature_start or physical_page > self.signature_end:
+            raise RuntimeError(f"Page {pageNumber} out of range ({self.signature_start} to {self.signature_end})")
+        return super().getPage(physical_page)
 
 
 def get_options():
@@ -43,6 +71,8 @@ def get_options():
                         help="[opt] destination paper size: {}".format(", ".join(page_sizes.keys())))
     parser.add_argument('--blank', type=int, default=0, required=False,
                         help="number of blank pages to append")
+    parser.add_argument('--signature', type=int, default=0, required=False,
+                        help="max number of pages per signature (must be a multiple of 4")
     parser.add_argument('--hoffset', type=str, required=False,
                         help="amount to reduce/increase distance from edge")
     parser.add_argument('--width', type=str, required=False,
@@ -58,29 +88,32 @@ def get_options():
 
     opt = parser.parse_args()
 
+    if opt.signature % 4 > 1:
+        raise ValueError("signature argument must be a multiple of 4!")
 
-def determine_booklet_page_counts(pdf_in):
+
+def determine_booklet_page_counts(sig_in):
     """
     Determines total pages needed and the front and back pointers needed for processing
-    :param pdf_in: PyPDF2 PDF_Reader object
+    :param sig_in: PdfSignatureReader object
     :return: tuple - (front_pointer, back_pointer, output_page_count)
     """
-    target_pages = pdf_in.numPages + opt.blank
+    target_pages = sig_in.signature_page_count + opt.blank
     front_pointer = 0
     back_pointer = target_pages - 1
     if target_pages % 4 > 0:
         back_pointer += 4 - target_pages % 4
     output_page_count = back_pointer + 1
-    logger.debug(f"source file has {pdf_in.numPages} pages, add blank={opt.blank} "
+    logger.debug(f"source signature has {sig_in.signature_page_count} pages, add blank={opt.blank} "
                  f"setting back_pointer to {back_pointer}")
-    logger.debug(f"XS Booklet will have {output_page_count} pages")
+    logger.debug(f"Signature will have {output_page_count} pages")
     return front_pointer, back_pointer, output_page_count
 
 
 def source_page_or_none(pdf_reader, page_number):
-    if page_number >= pdf_reader.numPages:
+    if page_number >= pdf_reader.signature_page_count:
         return None
-    return pdf_reader.getPage(page_number)
+    return pdf_reader.get_signature_page(page_number)
 
 
 def get_translation_offset(source_page, target_width, target_height):
@@ -121,14 +154,14 @@ def get_translation_offset(source_page, target_width, target_height):
     return x_offset, y_offset, scale
 
 
-def generate_large_booklet_pages(pdf_in):
+def generate_large_booklet_pages(sig_in):
     """
     Generates a new 2-up PDF page for booklet printing
-    :param pdf_in: Source PDF Reader object
+    :param sig_in: Source PDF Reader object
     :return: None
     """
 
-    front_pointer, back_pointer, output_page_count = determine_booklet_page_counts(pdf_in)
+    front_pointer, back_pointer, output_page_count = determine_booklet_page_counts(sig_in)
 
     # Calculate output paper values
     if opt.paper in page_sizes.keys():
@@ -142,15 +175,15 @@ def generate_large_booklet_pages(pdf_in):
     while front_pointer < back_pointer:
 
         # Get the front page object
-        front_page = pdf_in.getPage(front_pointer)
+        front_page = sig_in.get_signature_page(front_pointer)
 
         # Get the back page object or None if the pointer has not reached it yet.
-        if back_pointer >= pdf_in.numPages:
+        if back_pointer >= sig_in.signature_page_count:
             source = " (blank page)"
             back_page = None
         else:
             source = ""
-            back_page = pdf_in.getPage(back_pointer)
+            back_page = sig_in.get_signature_page(back_pointer)
 
         # Create a rotated new page
         target_page = PyPDF2.pdf.PageObject.createBlankPage(
@@ -187,14 +220,14 @@ def generate_large_booklet_pages(pdf_in):
         front_pointer += 1
 
 
-def generate_small_booklet_pages(pdf_in):
+def generate_small_booklet_pages(sig_in):
     """
     Generator function to yield each PDF page 8-up. Yields PDF Page ojbects
-    :param pdf_in: PDF Reader object
+    :param sig_in: PDFSignatureReader object
     :return:
     """
     # Get the normal booklet layout pointers and page count
-    front_pointer, back_pointer, output_page_count = determine_booklet_page_counts(pdf_in)
+    front_pointer, back_pointer, output_page_count = determine_booklet_page_counts(sig_in)
     # Calculate output paper values
     if opt.paper in page_sizes.keys():
         source_width, source_height = page_sizes[opt.paper]
@@ -217,7 +250,7 @@ def generate_small_booklet_pages(pdf_in):
     sheets = (output_page_count // 16) * 2
     if (output_page_count % 16) > 0:
         sheets += 2
-    logger.debug(f"XS Booklet will be printed onto {sheets} sheets")
+    logger.debug(f"Signature will be printed onto {sheets} sheets")
 
     # Create a list of pages in booklet order
     booklet_pages = []
@@ -228,8 +261,8 @@ def generate_small_booklet_pages(pdf_in):
             back_page_no = back_pointer - page_offset
             front_page_no = front_pointer + page_offset
             if front_page_no < back_page_no:
-                booklet_pages.append(source_page_or_none(pdf_in, back_page_no))
-                booklet_pages.append(source_page_or_none(pdf_in, front_page_no))
+                booklet_pages.append(source_page_or_none(sig_in, back_page_no))
+                booklet_pages.append(source_page_or_none(sig_in, front_page_no))
             else:
                 booklet_pages.extend((None, None))
 
@@ -238,8 +271,8 @@ def generate_small_booklet_pages(pdf_in):
             back_page_no = back_pointer - page_offset
             front_page_no = front_pointer + page_offset
             if front_page_no < back_page_no:
-                booklet_pages.append(source_page_or_none(pdf_in, front_page_no))
-                booklet_pages.append(source_page_or_none(pdf_in, back_page_no))
+                booklet_pages.append(source_page_or_none(sig_in, front_page_no))
+                booklet_pages.append(source_page_or_none(sig_in, back_page_no))
             else:
                 booklet_pages.extend((None, None))
 
@@ -280,12 +313,45 @@ def generate_small_booklet_pages(pdf_in):
                 x_location = column + x_offset
                 y_location = row + y_offset
                 # Translate and scale onto the target page
-                logger.debug(f"Generating page {pdf_in.getPageNumber(source_page) + 1} scale={scale:.2f} "
-                             f"x={x_location} y={y_location} sheet={sheet + 1}")
+                logger.debug(f"Generating page {sig_in.getPageNumber(source_page) + 1} scale={scale:.2f} "
+                              f"x={x_location} y={y_location} sheet={sheet + 1}")
                 target_page.mergeScaledTranslatedPage(source_page, scale, x_location, y_location)
 
         # Add the target page to the pdf writer object
         yield target_page
+
+
+def generate_signatures(input_file):
+    """
+    Generates signatures of pages.  If the max signature was not specified or the total pages of the PDF
+    are <= the max signature then we can just yeild the pdf reader object.  Otherwise yield each
+    PDFSignatureReader object for each range of signature pages.
+    :param input_file:
+    :return:
+    """
+
+    pdf_in = PyPDF2.PdfFileReader(input_file)
+
+    source_data = PdfData(pdf_in)
+    logger.debug(f"Source PDF name={opt.file_in} page_count={source_data.page_count}")
+
+    if opt.signature < 1:
+        logger.debug("signature option not supplied--single signature will be generated")
+        yield PdfSignatureReader(0, pdf_in.numPages - 1, input_file)
+    elif pdf_in.numPages <= opt.signature:
+        logger.debug("pages not greater than max signature--single signature will be generated")
+        yield PdfSignatureReader(0, pdf_in.numPages - 1, input_file)
+    else:
+        signature_number = 1
+        for start_page in range(0, pdf_in.numPages, opt.signature):
+            if start_page + opt.signature - 1 >= pdf_in.numPages:
+                end_page = pdf_in.numPages - 1
+            else:
+                end_page = start_page + opt.signature - 1
+            logger.debug(f"Generating signature number {signature_number} from pages {start_page}-{end_page}")
+            yield PdfSignatureReader(start_page, end_page, input_file)
+            signature_number += 1
+    return
 
 
 def main():
@@ -302,12 +368,6 @@ def main():
     # Input file context
     with open(opt.file_in, 'rb') as input_file:
 
-        pdf_in = PyPDF2.PdfFileReader(input_file)
-        pdf_out = PyPDF2.PdfFileWriter()
-
-        source_data = PdfData(pdf_in)
-        logger.debug(f"Source PDF name={opt.file_in} page_count={source_data.page_count}")
-
         if opt.size == 'large':
             page_generator = generate_large_booklet_pages
         elif opt.size == 'small':
@@ -317,9 +377,11 @@ def main():
 
         # Generate the new pages
         page_count = 0
-        for page in page_generator(pdf_in):
-            pdf_out.addPage(page)
-            page_count += 1
+        pdf_out = PyPDF2.PdfFileWriter()
+        for sig_in in generate_signatures(input_file):
+            for page in page_generator(sig_in):
+                pdf_out.addPage(page)
+                page_count += 1
 
         # Save the new file
         logger.debug("Writing new pages to output file")
